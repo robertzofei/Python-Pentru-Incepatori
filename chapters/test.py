2f45659c80761ba03d622bc2251d8dc6d26f50ae
robertzofei <rzofei@gmail.com>
2026-07-12 13:20:36 +0300

    new file:   chapters/functii.quarto_ipynb_3 	new file:   chapters/functii.quarto_ipynb_4 	new file:   chapters/ifElse.quarto_ipynb_3 	new file:   chapters/ifElse.quarto_ipynb_4 	modified:   chapters/test.py 	modified:   chapters/whileFor.qmd 	new file:   chapters/whileFor.quarto_ipynb_3 	new file:   chapters/whileFor.quarto_ipynb_4

--- chapters/test.py
+++ chapters/test.py
@@ -1,6 +1,13 @@
-number = 0
-while number < 10:
-    if number == 4:
-        break
-    number += 1
-    print(number)
+# Scenariu 1: range() pentru un număr fix de repetiții
+# Vrem să trimitem 3 ping-uri către un IP
+for i in range(3):
+    # i va lua valorile 0, 1, 2
+    print(f"Trimitere pachet de date numărul {i + 1}...")
+
+print("-" * 20)
+
+# Scenariu 2: for pentru a trece prin colecții de date
+departamente = ["IT", "HR", "Contabilitate"]
+
+for departament in departamente:
+    print(f"Generez raportul lunar pentru departamentul: {departament}")
